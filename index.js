8bce2bba7f80cfc292bea7684f4539548643e0d9
mkg20001 <mkg20001@gmail.com>
2018-02-27 15:14:36 +0100

    Fix codestyle and dependencies

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-'use strict';
+'use strict'
 
-var PromiseA = require('bluebird').Promise;
-var fs = PromiseA.promisifyAll(require('fs'));
-var crypto = require('crypto');
+const PromiseA = require('bluebird').Promise
+const fs = PromiseA.promisifyAll(require('fs'))
+const crypto = require('crypto')
 
-function noop() {
-}
+function noop () {}
 
-function create(options) {
-
+function create (options) {
   if (!options) {
-    options = {};
+    options = {}
   }
   if (!options.tmp) {
-    options.tmp = 'tmp';
+    options.tmp = 'tmp'
   }
   if (!options.bak) {
-    options.bak = 'bak';
+    options.bak = 'bak'
   }
   if (options.tmp === options.bak) {
-    throw new Error("'tmp' and 'bak' suffixes cannot be the same... duh");
+    throw new Error("'tmp' and 'bak' suffixes cannot be the same... duh")
   }
 
   var tmpnamefn = options.tmpnamefn || function (pathname) {
-    return pathname + '.' + crypto.randomBytes(8).toString('hex') + '.' + options.tmp;
-  };
+    return pathname + '.' + crypto.randomBytes(8).toString('hex') + '.' + options.tmp
+  }
   var baknamefn = options.baknamefn || function (pathname) {
-    return pathname + '.' + options.bak;
-  };
+    return pathname + '.' + options.bak
+  }
   /*
   var namefn = options.namefn || function (pathname) {
     return pathname;
@@ -37,64 +35,64 @@ function create(options) {
   var sfs = {
     writeFileAsync: function (filename, data, options) {
       return sfs.stage(filename, data, options).then(function (tmpname) {
-        //console.log(filename);
-        return sfs.commit(tmpname, filename);
-      });
-    }
-  , stageAsync: function (filename, data, options) {
-      var tmpname = tmpnamefn(filename);
-      //console.log(tmpname);
+        // console.log(filename);
+        return sfs.commit(tmpname, filename)
+      })
+    },
+    stageAsync: function (filename, data, options) {
+      var tmpname = tmpnamefn(filename)
+      // console.log(tmpname);
       return fs.writeFileAsync(tmpname, data, options).then(function () {
-        return tmpname;
-      });
-    }
-  , commitAsync: function (tmpname, filename) {
-      var bakname = baknamefn(filename);
+        return tmpname
+      })
+    },
+    commitAsync: function (tmpname, filename) {
+      var bakname = baknamefn(filename)
       // this may not exist
       return fs.unlinkAsync(bakname).then(noop, noop).then(function () {
         // this may not exist
-        //console.log(namefn(filename), '->', bakname);
+        // console.log(namefn(filename), '->', bakname);
         return fs.renameAsync(filename, bakname).then(function () {
-          //console.log('created bak');
-        }, noop);
+          // console.log('created bak');
+        }, noop)
       }).then(function () {
         // this must be successful
-        //console.log(filename, '->', filename);
+        // console.log(filename, '->', filename);
         return fs.renameAsync(tmpname, filename).then(noop, function (err) {
-          //console.error(err);
+          // console.error(err);
           return sfs.revert(filename).then(function () {
-            return PromiseA.reject(err);
-          });
-        });
-      });
-    }
-  , revertAsync: function (filename) {
+            return PromiseA.reject(err)
+          })
+        })
+      })
+    },
+    revertAsync: function (filename) {
       return new PromiseA(function (resolve, reject) {
-        var bakname = baknamefn(filename);
-        var tmpname = tmpnamefn(filename);
+        var bakname = baknamefn(filename)
+        var tmpname = tmpnamefn(filename)
 
-        var reader = fs.createReadStream(bakname);
-        var writer = fs.createWriteStream(tmpname);
+        var reader = fs.createReadStream(bakname)
+        var writer = fs.createWriteStream(tmpname)
 
-        reader.on('error', reject);
-        writer.on('error', reject);
+        reader.on('error', reject)
+        writer.on('error', reject)
 
-        reader.pipe(writer);
+        reader.pipe(writer)
         writer.on('close', function () {
-          sfs.commit(tmpname, filename).then(resolve, reject);
-        });
-      });
-    }
-  , tmpnamefn: tmpnamefn
-  , baknamefn: baknamefn
-  , create: create
-  };
-  sfs.writeFile = sfs.writeFileAsync;
-  sfs.stage = sfs.stageAsync;
-  sfs.commit = sfs.commitAsync;
-  sfs.revert = sfs.revertAsync;
+          sfs.commit(tmpname, filename).then(resolve, reject)
+        })
+      })
+    },
+    tmpnamefn: tmpnamefn,
+    baknamefn: baknamefn,
+    create: create
+  }
+  sfs.writeFile = sfs.writeFileAsync
+  sfs.stage = sfs.stageAsync
+  sfs.commit = sfs.commitAsync
+  sfs.revert = sfs.revertAsync
 
-  return sfs;
+  return sfs
 }
 
-module.exports = create();
+module.exports = create()
--- test.js
+++ test.js
@@ -1,11 +1,11 @@
-'use strict';
+'use strict'
 
-var safeReplace = require('./'); //.create();
-//var safeReplace = require('safe-replace').create();
-var fs = require('fs');
+var safeReplace = require('./') // .create();
+// var safeReplace = require('safe-replace').create();
+var fs = require('fs')
 
 safeReplace.writeFile('keep.txt', 'my precious').then(function () {
   fs.readdir('.', function (err, nodes) {
-    console.log('file system nodes', nodes);
-  });
-});
+    console.log('file system nodes', nodes)
+  })
+})
